0264fbd753c655beb902f59214df6d4a0a1c62ff
Faiz Alam <faiz276482@gmail.com>
2019-11-03 15:50:12 +0530

    Added image upload feature

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,10 +9,20 @@
 package com.parse.starter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -26,11 +36,111 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener,View.OnKeyListener {
+
+    public void showUserList()
+    {
+        Intent intent=new Intent(getApplicationContext(),UserListActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId()==R.id.loginTextView)
+        {
+            //Log.i("Login was Pressed","Succesfully");
+            signUp=findViewById(R.id.signUpButton);
+            if(signupmodeactive)
+            {
+                signupmodeactive=false;
+                signUp.setText("Login");
+                loginTextView.setText("or SignUp");
+
+            }
+            else {
+                signupmodeactive=true;
+                signUp.setText("SignUp");
+                loginTextView.setText("or Login");
+            }
+        }
+        else if(view.getId()==R.id.backgroundLayout || view.getId()==R.id.logoImageView)
+        {
+            InputMethodManager inputMethodManager=(InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+        }
+    }
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        if(i==KeyEvent.KEYCODE_ENTER && keyEvent.getAction()==KeyEvent.ACTION_DOWN)
+        {
+            signUpclicked(view);
+        }
+
+        return false;
+    }
+
+    Boolean signupmodeactive= true;
+    TextView loginTextView;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button signUp;
+    ConstraintLayout backgroundLayout;
+    ImageView logoImageView;
+    public void signUpclicked(View view){
+        //Log.i("Info:","Sihnup pressed");
+
+
+
+        if(usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals(""))
+        {
+            Toast.makeText(this, "Username or password empty", Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            ParseUser user=new ParseUser();
+            user.setUsername(usernameEditText.getText().toString());
+            user.setPassword(passwordEditText.getText().toString());
+
+            if(signupmodeactive) {
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("Signup", "SUCCESSFUL");
+                            showUserList();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+            else{
+                user.logInInBackground((usernameEditText.getText().toString()), (passwordEditText.getText().toString()), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user!= null) {
+                            Log.i("Login", "SUCCESSFUL");
+                            showUserList();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
 
 
   @Override
@@ -38,7 +148,26 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ParseUser user=new ParseUser();
+    loginTextView=findViewById(R.id.loginTextView);
+    loginTextView.setOnClickListener(this);
+      usernameEditText=findViewById(R.id.nameEditText);
+      passwordEditText=findViewById(R.id.passwordEditText);
+      logoImageView=findViewById(R.id.logoImageView);
+      backgroundLayout=findViewById(R.id.backgroundLayout);
+
+      logoImageView.setOnClickListener(this);
+      backgroundLayout.setOnClickListener(this);
+
+      passwordEditText.setOnKeyListener(this);
+
+
+
+    //ParseUser user=new ParseUser();
+
+    if(ParseUser.getCurrentUser()!=null)
+    {
+        showUserList();
+    }
       /*
       //Signup Code
       user.setUsername("Anjali");
@@ -83,13 +212,14 @@ public class MainActivity extends AppCompatActivity {
         }
       });
       */
+      /*
       //Checking if a user is currently logged in or not
       if(ParseUser.getCurrentUser()!=null){
         System.out.println("Signed in:"+"Username= "+ ParseUser.getCurrentUser().getUsername());
       }
       else {
         System.out.println("User needs to Login");
-      }
+      }*/
 
     /*
     ParseQuery<ParseObject>query =ParseQuery.getQuery("Score");
---
+++ ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
@@ -0,0 +1,165 @@
+package com.parse.starter;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserListActivity extends AppCompatActivity {
+
+    public  void getPhoto()
+    {
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode==1)
+        {
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+            {
+                getPhoto();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.share_menu,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.share)
+        {
+
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            }
+            else {
+                getPhoto();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+
+        listView=findViewById(R.id.listView);
+        final ArrayList<String> usernames=new ArrayList<String>();
+        //usernames.add("test");
+        final ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(getApplication(),android.R.layout.simple_list_item_1,usernames);
+        listView.setAdapter(arrayAdapter);
+
+        ParseQuery<ParseUser> query=ParseUser.getQuery();
+        query.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        query.addAscendingOrder("username");
+
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if(e==null)
+                {
+                    if(objects.size()>0)
+                    {
+                        for(ParseUser user: objects)
+                        {
+                            usernames.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+                else{
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri selectedImage=data.getData();
+        if(requestCode==1 && resultCode==RESULT_OK && data !=null)
+        {
+            try
+            {
+                Bitmap bitmap=MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImage);
+                //ImageView imageView=findViewById(R.id.imageView);
+                //imageView.setImageBitmap(bitmap);
+
+                Log.i("Image Selected","Great job!");
+                ByteArrayOutputStream stream =new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+                byte[] byteArray=stream.toByteArray();
+
+                ParseFile file=new ParseFile("image.png",byteArray);
+                ParseObject object=new ParseObject("Image");
+
+                object.put("image",file);
+                object.put("username",ParseUser.getCurrentUser().getUsername());
+                object.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null)
+                        {
+                            Toast.makeText(UserListActivity.this, "Image Have been shared!", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(UserListActivity.this, "There has been an issue uploading an image :( ", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
